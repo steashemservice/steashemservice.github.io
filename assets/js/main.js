525f844b160ad85b6fb9006b442222aa06d913d5
steashemservice <32518087+steashemservice@users.noreply.github.com>
2019-09-14 17:52:55 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -425,7 +425,9 @@ var contactFormHost = 'https://app.steas.se/user_interest_recycle.php',
           }
         },
         error: function(xhr, ajaxOptions, thrownError) {
-          notice.html('<h4>' + notice.data('exception') + '</h4>').fadeIn();
+          var err = eval("(" + xhr.responseText + ")");
+          //notice.html('<h4>' + notice.data('exception') + '</h4>').fadeIn();
+	  notice.html('<h4>' + err + '</h4>').fadeIn();
         }
       });
     });
